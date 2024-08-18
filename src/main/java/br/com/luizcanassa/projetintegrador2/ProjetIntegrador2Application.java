da2ab835f5219ac62ad862dd213074b8a5676ce7
Luiz Henrique Canassa <luizcanassa@hotmail.com>
2024-08-18 19:46:00 -0300

    Commit Inicial

---
+++ src/main/java/br/com/luizcanassa/projetintegrador2/ProjetIntegrador2Application.java
@@ -0,0 +1,13 @@
+package br.com.luizcanassa.projetintegrador2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIntegrador2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetIntegrador2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luizcanassa/projetintegrador2/ProjetIntegrador2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizcanassa.projetintegrador2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIntegrador2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
