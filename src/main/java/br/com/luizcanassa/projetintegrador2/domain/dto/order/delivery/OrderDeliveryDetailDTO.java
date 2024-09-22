3224cd29d85b65eccb0f28ab2c42bf127badce16
Luiz Henrique Canassa <luizcanassa@hotmail.com>
2024-09-22 16:53:42 -0300

    Detalhes e alteração de status pedido delivery

---
+++ src/main/java/br/com/luizcanassa/projetintegrador2/domain/dto/order/delivery/OrderDeliveryDetailDTO.java
@@ -0,0 +1,24 @@
+package br.com.luizcanassa.projetintegrador2.domain.dto.order.delivery;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class OrderDeliveryDetailDTO {
+
+    private Long id;
+
+    private String document;
+
+    private String address;
+
+    private String details;
+
+    private String orderItems;
+
+    private String status;
+
+    private BigDecimal totalAmount;
+
+}
